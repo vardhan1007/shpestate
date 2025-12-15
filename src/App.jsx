c27ba5305316d7dd090433588ea1dc9eec62fa21
vardhan1007 <svardhan1007@gmail.com>
2025-12-15 06:20:17 +0530

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Buy from './pages/Buy'
+import Sell from './pages/Sell'
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-black text-red-500 text-4xl">
-      TAILWIND TEST
-    </div>
-  )
+  return(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/buy' element={<Buy/>}/>
+      <Route path='/sell' element={<Sell/>}/>
+    </Routes>
+  </BrowserRouter>
+  )  
 }
 
